6fc21bcbb01d532ed88031a52d2c1cf14ea449fa
hauzerlin <zaq60509@gmail.com>
2022-10-11 21:24:56 +0800

    result1 1.0

--- VIVADO/Lab2/serial_comparator2/serial_comparator2.srcs/sim_1/new/feed_test_tb.v
+++ VIVADO/Lab2/serial_comparator2/serial_comparator2.srcs/sim_1/new/feed_test_tb.v
@@ -11,35 +11,79 @@ module feed_test_tb;
     
     `define central_pattern "../../../../rand32.dat"
     // Inputs
-    reg signed [7:0] central_pat_mem [0:31];
     reg clk=0;
+    reg signed [7:0] central_pat_mem [0:31];
+    reg signed [12:0] mat_32 [0:31];
+    reg [7:0] mat32_values;
+    reg [4:0] mat32_indexes;
     reg [12:0] in_value_a, in_value_b;
+    parameter WIDTH = 32;
+    integer i=32,j,clk_en,comp_en=0;
     // Outputs
     wire compare_result;
     wire [12:0] out_value;
     
-    lab2_top max_com(.clk(clk), .in_value_a(in_value_a), .in_value_b(in_value_b), .out_value(out_value));
+    lab2_top min_com(.clk(clk), .in_value_a(in_value_a), .in_value_b(in_value_b), .out_value(out_value) ,.compare_result(compare_result));
     
     initial begin
         $readmemh(`central_pattern, central_pat_mem);
         end
     
     always  #5 clk = ~clk;
+    
+    always @(posedge clk)
+    begin
+        if(clk_en)
+        begin
+                for (j = 0; j < WIDTH-1; j = j+1)begin
+                    mat_32[j+1] <= mat_32[j];
+                end
+                mat_32[0][12:5] <= central_pat_mem[i];
+                mat_32[0][4:0] <= i+1;
+                mat32_values <= central_pat_mem[i];
+                mat32_indexes <= i+1;
+                i = i-1;
+        end
+   end
+    
     initial begin
-    in_value_a[12:5] <= central_pat_mem[0];//8'd14;
-    in_value_a[4:0] <= 5'd1;
+    clk_en <= 1;
+    #330
+    clk_en <= 0;
     
-    in_value_b[12:5] <= -8'd75;
-    in_value_b[4:0] <= 5'd2;
-    
-    #30
-    
-    in_value_a[12:5] <= 8'd25;
-    in_value_a[4:0] <= 5'd3;
-    
-    in_value_b[12:5] <= -8'd20;
-    in_value_b[4:0] <= 5'd4;
+    i = 1;
+    in_value_a <= mat_32[i-1];//8'd14;
+    in_value_b <= mat_32[i];
+    #10
+    comp_en<= 1;
+    #330
+    comp_en<=0;
     end
     
+    always @(posedge clk)
+    begin
+        if(comp_en)  
+        begin
+        in_value_a <= out_value;//8'd14;
+        in_value_b <= mat_32[i];
+        i = i+1;
+        end  
+    end 
+        
+//    initial begin
+//    in_value_a[12:5] <= 8'd14;//8'd14;
+//    in_value_a[4:0] <= 5'd1;
+    
+//    in_value_b[12:5] <= -8'd75;
+//    in_value_b[4:0] <= 5'd2;
+    
+//    #10
+    
+//    in_value_a[12:5] <= 8'd25;
+//    in_value_a[4:0] <= 5'd3;
+    
+//    in_value_b[12:5] <= -8'd20;
+//    in_value_b[4:0] <= 5'd4;
+//    end
 
 endmodule
--- VIVADO/Lab2/serial_comparator2/serial_comparator2.srcs/sources_1/new/min_comparator.v
+++ VIVADO/Lab2/serial_comparator2/serial_comparator2.srcs/sources_1/new/min_comparator.v
@@ -7,15 +7,15 @@ module min_comparator(in_value_a, in_value_b, compare_result, out_value);
 input signed [12:0] in_value_a; // value a + index 8+5 bits
 input signed [12:0] in_value_b; // value b+ index 8+5 bits
 output reg  compare_result; // binary 1 or 0
-output reg [12:0] out_value; // value+index
+output reg signed [12:0] out_value; // value+index
 
 always @(*)
 begin
-if(in_value_a[12:5]<in_value_b[12:5]) compare_result <= 1;
-else  compare_result <= 0;
+if(in_value_b<in_value_a) compare_result = 1;
+else  compare_result = 0;
 
-if(compare_result) out_value <= in_value_a;
-else out_value <= in_value_b;
+if(compare_result) out_value <= in_value_b;
+else out_value <= in_value_a;
        
 end
 
