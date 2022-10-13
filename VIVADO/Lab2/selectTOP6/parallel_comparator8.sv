7d3ae338da195ce2aa60a2a5f22813982f3f36be
hauzerlin <zaq60509@gmail.com>
2022-10-13 22:54:39 +0800

    lab2 verilog complete

---
+++ VIVADO/Lab2/selectTOP6/parallel_comparator8.sv
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+
+module parallel_comparator8(in_mat_8, out_mat_4, compare_result_4);
+    // Inputs
+    input signed [12:0] in_mat_8 [0:7];
+//    input clk;
+    // Outputs
+    output signed [12:0] out_mat_4 [0:3];
+    
+   output [15:0] compare_result_4;
+
+    
+//    module min_comparator(in_value_a, in_value_b, compare_result, out_value);
+
+    generate
+        genvar idx;
+        for (idx = 0; idx < 4; idx = idx+1)
+        begin
+         max_comparator layer1        ( //.clk                ( clk),
+                                        .in_value_a         ( in_mat_8[idx*2]              ),
+                                        .in_value_b         ( in_mat_8[idx*2+1]             ),
+                                        .compare_result     ( compare_result_4[4-idx]   ),
+                                        .out_value          ( out_mat_4[idx])             );
+        end        
+    endgenerate   
+                                  
+endmodule
